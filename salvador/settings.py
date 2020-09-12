abc642be8d7baec0741d795ec03cc7afb3fdadf2
Szymon <korzonek.szymon@gmail.com>
2020-09-12 14:05:32 +0200

    another commit

--- salvador/settings.py
+++ salvador/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7iljjntrn_1xgxjzdm(_s1oz_o88xsn57_tig@4lanl*%fl)z&'
+SECRET_KEY = os.environ['SALVATOR_SECRET_KEY']
+#SECRET_KEY = '7iljjntrn_1xgxjzdm(_s1oz_o88xsn57_tig@4lanl*%fl)z&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
