2e4da0bd3adae5360538ae7e2810e292c729fe2b
Szymon <korzonek.szymon@gmail.com>
2020-09-26 12:13:53 +0200

    added readme, typos fixes

--- salvator/wsgi.py
+++ salvator/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for salvador project.
+WSGI config for salvator project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'salvador.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'salvator.settings')
 
 application = get_wsgi_application()
